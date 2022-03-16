b7b203cd0d4f8eff95e9c411006d0c47379b1ef5
haagmin5382 <itworker5382@gmail.com>
2022-03-16 22:04:23 +0900

    ✨ feat : add StoredRepoContainer

--- src/pages/Issue.jsx
+++ src/pages/Issue.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Gnb from '../components/Gnb';
 import { MainWrap } from './Main';
 import StoredRepoContainer from '../components/StoredRepoContainer';
-import StoredRepository from '../components/StoredRepository';
-
+import NotificationMessage from '../components/NotificationMessage';
 const Issue = () => {
   return (
     <MainWrap>
       <Gnb />
       <div>컴포넌트</div>
       <StoredRepoContainer />
+      <NotificationMessage />
     </MainWrap>
   );
 };
