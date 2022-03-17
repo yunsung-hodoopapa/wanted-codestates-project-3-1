b6d7dd95cc203f21f0b8cfd406df9df5a6a59032
NamgyungKim <namgyung.kim@gmail.com>
2022-03-17 16:34:49 +0900

    ✨ feat: 이미 저장한 repo 다시 저장 못하도록 했습니다.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -11,7 +11,7 @@ const List = ({ type = 'repo', item, clickHandle }) => {
   let itemId, repoName, htmlUrl, imgUrl, title, text, date;
   let owner_id, owner_name;
 
-  const StoredData = useSelector(state => state.data.store);
+  const storedData = useSelector(state => state.data.store);
 
   if (type === 'issue') {
     const issue = item;
@@ -48,7 +48,8 @@ const List = ({ type = 'repo', item, clickHandle }) => {
   };
 
   const saveRepo = () => {
-    if (StoredData.length >= 4) {
+    if (isSave(itemId)) return;
+    if (storedData.length >= 4) {
       dispatch(notify('repository 저장 개수를 초과했습니다.', 1500));
     } else {
       dispatch(
@@ -70,6 +71,14 @@ const List = ({ type = 'repo', item, clickHandle }) => {
     dispatch(deleteRepo(itemId));
   };
 
+  const isSave = id => {
+    return storedData.some(data => {
+      if (data.id === id) {
+        return true;
+      }
+    });
+  };
+
   return (
     <Box type={type}>
       <Content onClick={onClickEvent}>
@@ -81,7 +90,14 @@ const List = ({ type = 'repo', item, clickHandle }) => {
         </div>
       </Content>
       <Option>
-        {type === 'repo' ? <button onClick={saveRepo}>저 장</button> : null}
+        {type === 'repo' ? (
+          <button
+            onClick={saveRepo}
+            className={isSave(itemId) ? 'registered' : null}
+          >
+            저 장
+          </button>
+        ) : null}
         {type === 'issue' ? <p>{repoName}</p> : null}
         {type === 'stored' ? <i onClick={removeRepo}></i> : null}
       </Option>
@@ -169,6 +185,11 @@ const Option = styled.div`
   .registered {
     color: #8b8c93;
     background-color: #d4d5dd;
+    cursor: default;
+    &:hover {
+      color: #8b8c93;
+      background-color: #d4d5dd;
+    }
   }
   //repoName
   p {
